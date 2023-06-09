0b181b39bde67c05e22cbacb80452cafb0dfa2ee
alex.s <shikorin@mail.ru>
2023-06-09 11:50:35 +0300

    fix: key

--- src/components/ProductList.vue
+++ src/components/ProductList.vue
@@ -1,6 +1,6 @@
 <template>
   <ul class="catalog__list">
-    <ProductItem v-for="(product, index) in products" :product="product" :key="index" />
+    <ProductItem v-for="product in products" :product="product" :key="product.id" />
   </ul>
 </template>
 
