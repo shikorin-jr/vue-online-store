59b5981e263dd4bbb76f649e94ddd67c325b6545
alex.s <shikorin@mail.ru>
2023-06-09 10:55:22 +0300

    feat: add pagination component

--- src/components/ProductItem.vue
+++ src/components/ProductItem.vue
@@ -17,7 +17,13 @@
     <ul class="colors colors--black">
       <li class="colors__item">
         <label class="colors__label">
-          <input class="colors__radio sr-only" type="radio" name="color-1" value="#73B6EA" checked="">
+          <input
+            class="colors__radio sr-only"
+            type="radio"
+            name="color-1"
+            value="#73B6EA"
+            checked=""
+          >
           <span class="colors__value" style="background-color: #73B6EA;">
                   </span>
         </label>
