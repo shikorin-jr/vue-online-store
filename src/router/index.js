863c6e182ca8e632fa3216b29e0c19d5ec2667ed
alex.s <shikorin@mail.ru>
2023-06-18 09:59:27 +0300

    feat: vue router

---
+++ src/router/index.js
@@ -0,0 +1,20 @@
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import MainPage from '@/pages/MainPage.vue';
+import NotFoundPage from '@/pages/NotFoundPage.vue';
+import ProductPage from '@/pages/ProductPage.vue';
+import CarPage from '@/pages/CarPage.vue';
+
+Vue.use(VueRouter);
+
+const routes = [
+  { name: 'main', component: MainPage, path: '/' },
+  { name: 'product', component: ProductPage, path: '/product/:id' },
+  { name: 'cart', component: CarPage, path: '/cart' },
+  { name: 'notFound', component: NotFoundPage, path: '*' },
+];
+
+const router = new VueRouter({
+  routes,
+});
+export default router;
