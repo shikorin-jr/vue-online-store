263f12ebe1059252c9460a2fff748cc8baae7e53
alex.s <shikorin@mail.ru>
2023-06-23 15:49:50 +0300

    feat: add data products api and pagination

--- src/pages/MainPage.vue
+++ src/pages/MainPage.vue
@@ -30,6 +30,7 @@ import products from '@/data/products';
 import ProductList from '@/components/ProductList.vue';
 import BasePagination from '@/components/BasePagination.vue';
 import ProductFilter from '@/components/ProductFilter.vue';
+import axios from 'axios';
 
 export default {
   components: { ProductFilter, BasePagination, ProductList },
@@ -41,6 +42,8 @@ export default {
 
       page: 1,
       productsPerPage: 3,
+
+      productsData: null,
     };
   },
   computed: {
@@ -63,15 +66,38 @@ export default {
       return filteredProducts;
     },
     products() {
-      // Начальный индекс
-      const offset = (this.page - 1) * this.productsPerPage;
-      // return products.slice(offset, offset + this.productsPerPage);
-      return this.filteredProducts.slice(offset, offset + this.productsPerPage);
+      // // Начальный индекс
+      // const offset = (this.page - 1) * this.productsPerPage;
+      // // return products.slice(offset, offset + this.productsPerPage);
+      // return this.filteredProducts.slice(offset, offset + this.productsPerPage);
+      return this.productsData
+        ? this.productsData.items.map((product) => ({
+          ...product,
+          image: product.image.file.url,
+        }))
+        : [];
     },
     countProducts() {
       // return products.length;
-      return this.filteredProducts.length;
+      // // return this.filteredProducts.length;
+      return this.productsData ? this.productsData.pagination.total : 0;
     },
+  },
+  methods: {
+    loadProducts() {
+      axios.get(`https://vue-study.skillbox.cc/api/products?page=${this.page}&limit=${this.productsPerPage}`)
+        .then((response) => {
+          this.productsData = response.data;
+        });
+    },
+  },
+  watch: {
+    page() {
+      this.loadProducts();
+    },
+  },
+  created() {
+    this.loadProducts();
   },
 };
 </script>
