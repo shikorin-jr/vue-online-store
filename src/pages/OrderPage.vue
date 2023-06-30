41919c697839f1316b23ab6a768e8a9bda72b582
alex.s <shikorin@mail.ru>
2023-06-30 14:13:08 +0300

    feat: Successfully placed order page

--- src/pages/OrderPage.vue
+++ src/pages/OrderPage.vue
@@ -183,8 +183,10 @@ export default {
             userAccessKey: this.$store.state.userAccessKey,
           },
         })
-        .then(() => {
+        .then((response) => {
           this.$store.commit('resetCart');
+          this.$store.commit('updateOrderInfo', response.data);
+          this.$router.push({ name: 'orderInfo', params: { id: response.data.id } });
         })
         .catch((error) => {
           this.formError = error.response.data.error.request || {};
--- src/store/index.js
+++ src/store/index.js
@@ -12,8 +12,13 @@ export default new Vuex.Store({
     userAccessKey: null,
 
     cartProductsData: [],
+
+    orderInfo: null,
   },
   mutations: {
+    updateOrderInfo(state, orderInfo) {
+      state.orderInfo = orderInfo;
+    },
     resetCart(state) {
       state.cartProducts = [];
       state.cartProductsData = [];
@@ -63,6 +68,17 @@ export default new Vuex.Store({
     },
   },
   actions: {
+    loadOrderInfo(context, orderId) {
+      return axios
+        .get(`${API_BASE_URL}/api/orders/${orderId}`, {
+          params: {
+            userAccessKey: context.state.userAccessKey,
+          },
+        })
+        .then((response) => {
+          context.commit('updateOrderInfo', response.data);
+        });
+    },
     loadCart(context) {
       return axios
         .get(`${API_BASE_URL}/api/baskets`, {
